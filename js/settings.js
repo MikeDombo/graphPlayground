7b1fb0ff529e566e7e54192a942a6d7b8dbbd4ca
Michael Dombrowski <mikepi3.14@gmail.com>
2017-11-29 23:58:27 -0500

    Fix reset to default

--- js/settings.js
+++ js/settings.js
@@ -1,4 +1,4 @@
-define([], () => {
+define([], () =>{
 	let self = {
 		defaults: {
 			nodePhysics: true,
@@ -16,7 +16,7 @@ define([], () => {
 			}
 			self.setAll();
 		},
-		setAll: function(){
+		setAll: function (){
 			network.setOptions({nodes: {physics: self.getOption("nodePhysics")}});
 			network.setOptions({edges: {arrows: {to: self.getOption("direction")}}});
 			if(self.getOption("weights")){
@@ -28,7 +28,7 @@ define([], () => {
 					}
 				});
 			}
-			else {
+			else{
 				network.setOptions({manipulation: {editEdge: main.visOptions.manipulation.editEdge}});
 			}
 		},
@@ -37,16 +37,21 @@ define([], () => {
 			self.saveSettings();
 			self.setAll();
 		},
-		getOption: function(option){
+		getOption: function (option){
 			if(option in self.current){
 				return self.current[option];
 			}
 			return self.defaults[option];
 		},
-		resetToDefault: function(){
+		resetToDefault: function (){
 			self.current = {};
 			self.saveSettings();
 			self.setAll();
+
+			// Reset graph to just a plain graph. Not sure if this should actually happen or not.
+			let d = main.graphState.getGraphData();
+			main.setData(main.graphState.getGraphData(main.graphState.dataSetToGraph(d.nodes, d.edges, true, false, self.defaults.direction, self.defaults.weights)));
+
 		},
 		makeSettingsPanel: function (){
 
