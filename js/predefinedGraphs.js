75a63ccd6e6d22b14441984327e94aa291db6647
Michael Dombrowski <mikepi3.14@gmail.com>
2017-12-01 22:50:14 -0500

    Add complete and hypercube graphs

--- js/predefinedGraphs.js
+++ js/predefinedGraphs.js
@@ -1,4 +1,4 @@
-define(["graphHelpers", "vis"], (gHelp, vis) => {
+define(["graphHelpers", "vis", "genericHelpers"], (gHelp, vis, help) => {
 	let petersenEdges = [
 		{from: 1, to: 2},
 		{from: 2, to: 3},
@@ -29,8 +29,59 @@ define(["graphHelpers", "vis"], (gHelp, vis) => {
 		{from: 4, to: 1},
 	];
 
+	let completeGraph = (V) => {
+		let edges = [];
+		let nodes = [];
+
+		for(let i = 0; i < V; i++){
+			nodes.push({id: i, label: i.toString()});
+			for(let j = i + 1; j < V; j++){
+				edges.push({from: i, to: j});
+			}
+		}
+
+		return {nodes: nodes, edges: edges, directed: false, weighted: false};
+	};
+
+	let hypercubeGraph = (D) => {
+		let edges = [];
+		let nodes = [];
+
+		let numNodes = Math.pow(2, D);
+
+		let pad = (str, max) => {
+			return str.length < max ? pad("0" + str, max) : str;
+		};
+
+		let generateDifferByOne = (input, numBits) => {
+			let inputBits = pad((input).toString(2), numBits).split("").reverse();
+			let original = inputBits.slice(); // Save original
+			let allDiffer = [];
+
+			// 1 bit difference from input, increasing order, none less than input
+			//                               Replace original input bits
+			for(let b = 0; b < numBits; b++, inputBits = original){
+				if(inputBits[b] === "0"){
+					inputBits[b] = "1";
+					allDiffer.push(parseInt(inputBits.reverse().join(""), 2));
+				}
+			}
+
+			return allDiffer;
+		};
+
+		for(let i = 0; i < numNodes; i++){
+			nodes.push({id: i, label: pad(i.toString(2), D)});
+			generateDifferByOne(i, D).forEach((j) => {
+				edges.push({from: i, to: j});
+			});
+		}
+
+		return {nodes: nodes, edges: edges, directed: false, weighted: false};
+	};
+
 	return {
-		graphNames: ["Petersen", "Konigsberg"],
+		graphNames: ["Petersen", "Konigsberg", "Complete", "Hypercube"],
 		Petersen: () => ({
 			edges: new vis.DataSet(petersenEdges),
 			nodes: gHelp.interpolateNodesFromEdges(petersenEdges),
@@ -43,5 +94,18 @@ define(["graphHelpers", "vis"], (gHelp, vis) => {
 			directed: false,
 			weighted: false,
 		}),
+		Complete: () => {
+			help.showFormModal(($modal, vals) => {
+				$modal.modal("hide");
+				main.setData(completeGraph(parseInt(vals[0])));
+			}, "Configurable Complete Graph", "Go", [{type: "numeric", initialValue: 5, label: "Number of Vertices"}]);
+		},
+		Hypercube: () => {
+			help.showFormModal(($modal, vals) => {
+					$modal.modal("hide");
+					main.setData(hypercubeGraph(parseInt(vals[0])));
+				}, "Configurable Hypercube Graph", "Go",
+				[{type: "numeric", initialValue: 3, label: "Number of Dimensions"}]);
+		},
 	};
 });
