3d511a1e64c6b673f86df82c1503fe0b8e11e948
Michael Dombrowski <mikepi3.14@gmail.com>
2018-03-26 13:34:36 -0400

    Add Babel for older browsers

--- webpack.config.js
+++ webpack.config.js
@@ -4,28 +4,53 @@ const CopyWebpackPlugin = require('copy-webpack-plugin');
 const outputPath = path.resolve(__dirname, 'dist');
 
 module.exports = {
-	entry: {
-		bundle:'./src/js/app.js',
-		pwaPacked: './src/pwaServiceWorker.js'
-	},
-	output: {
-		filename: '[name].js',
-		path: outputPath
-	},
-	plugins: [
-		new CopyWebpackPlugin([
-			{
-				from: '*.png'
-			},
-			{
-				from: '*.ico'
-			},
-			{
-				from: './index.html'
-			},
-			{
-				from: './manifest.json'
-			}
-		])
-	]
+    entry: {
+        bundle: './src/js/app.js',
+        pwaPacked: './src/pwaServiceWorker.js'
+    },
+    output: {
+        filename: '[name].js',
+        path: outputPath
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            ['env', {
+                                targets: {
+                                    browsers: ["last 2 versions", "safari >= 9"]
+                                },
+                                "useBuiltIns": true
+                            }]
+                        ]
+                    }
+                }
+            }
+        ]
+    },
+    stats: {
+        colors: true
+    },
+    devtool: "source-map",
+    plugins: [
+        new CopyWebpackPlugin([
+            {
+                from: '*.png'
+            },
+            {
+                from: '*.ico'
+            },
+            {
+                from: './index.html'
+            },
+            {
+                from: './manifest.json'
+            }
+        ])
+    ]
 };
