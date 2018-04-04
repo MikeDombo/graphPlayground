3468f1f726daaf31a120667dac5e5fb531369ad8
Michael Dombrowski <mikepi3.14@gmail.com>
2018-04-04 18:35:43 -0400

    dependency bump

--- src/js/settings.ts
+++ src/js/settings.ts
@@ -10,7 +10,7 @@ interface SettingsList {
 }
 
 export default class Settings {
-    private static defaults: SettingsList = {
+    private static readonly defaults: SettingsList = {
         nodePhysics: true,
         direction: false,
         weights: false
